ee7f9b15e55db3f0ad58f8ad165250a431279249
Monica Claire W. <101022450+monicacw21@users.noreply.github.com>
2024-12-24 08:43:31 -0600

    -Created initial parameter tuning.

---
+++ models/parameter_tuning/__init__.py
@@ -0,0 +1,4 @@
+"""
+Module to initialize parameter tuning.
+"""
+
---
+++ models/parameter_tuning/parameter_tuning_processor.py
@@ -0,0 +1,40 @@
+"""
+Abstract module for processing parameter tuning.
+"""
+
+from abc import ABC, abstractmethod
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import utilities as utilities
+from models.models_data import ModelsData
+
+
+class ParameterTuningProcessor(ABC):
+    """
+    Abstract base class for creating portfolio signals.
+    """
+
+    def __init__(self, models_data: ModelsData):
+        """
+        Initializes the SignalProcessor class.
+
+        Parameters
+        ----------
+        models_data : object
+            An instance of the ModelsData class that holds all necessary attributes.
+        """
+
+    def process(self):
+        """
+        Abstract method to process data and generate trading signals.
+        """
+        self.process_parameters()
+
+
+    @abstractmethod
+    def process_parameters(self):
+        """
+        Abstract method to generate trading signals.
+        """
