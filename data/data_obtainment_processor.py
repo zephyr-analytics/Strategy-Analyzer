e44679e97be7d6b0a6fa7b1fcf155979f973ec09
Monica Claire W. <101022450+monicacw21@users.noreply.github.com>
2025-01-11 12:07:46 -0600

    -Update linting erorrs data obtain.

--- data/data_obtainment_processor.py
+++ data/data_obtainment_processor.py
@@ -4,10 +4,12 @@ Module for obtaining and saving data to the raw directory.
 
 import logging
 import os
+from datetime import datetime
+
 import pandas as pd
-from datetime import datetime, timedelta
+
+import utilities
 from logger import logger
-import utilities as utilities
 from models.models_data import ModelsData
 
 logger = logging.getLogger(__name__)
@@ -20,14 +22,7 @@ class DataObtainmentProcessor:
 
     def __init__(self, models_data: ModelsData):
         self.data_models = models_data
-        self.weights_filename = self.data_models.weights_filename
         self.end_date = pd.to_datetime(self.data_models.end_date)
-        self.asset_weights = self.data_models.assets_weights
-        self.cash_ticker = self.data_models.cash_ticker
-        self.bond_ticker = self.data_models.bond_ticker
-        self.ma_threshold_asset = self.data_models.ma_threshold_asset
-        self.benchmark_asset = self.data_models.benchmark_asset
-        self.out_of_market_tickers = self.data_models.out_of_market_tickers
 
     def process(self):
         """
@@ -56,23 +51,23 @@ class DataObtainmentProcessor:
             logger.info("Raw data file not found. Creating a new one.")
             df = pd.DataFrame()
 
-        all_tickers = set(self.asset_weights.keys())
+        all_tickers = set(self.data_models.assets_weights.keys())
 
-        if self.cash_ticker:
-            all_tickers.add(self.cash_ticker)
-        if self.bond_ticker:
-            all_tickers.add(self.bond_ticker)
-        if self.ma_threshold_asset:
-            all_tickers.add(self.ma_threshold_asset)
-        if self.benchmark_asset:
-            all_tickers.add(self.benchmark_asset)
-        if self.out_of_market_tickers:
-            all_tickers.update(self.out_of_market_tickers.keys())
+        if self.data_models.cash_ticker:
+            all_tickers.add(self.data_models.cash_ticker)
+        if self.data_models.bond_ticker:
+            all_tickers.add(self.data_models.bond_ticker)
+        if self.data_models.ma_threshold_asset:
+            all_tickers.add(self.data_models.ma_threshold_asset)
+        if self.data_models.benchmark_asset:
+            all_tickers.add(self.data_models.benchmark_asset)
+        if self.data_models.out_of_market_tickers:
+            all_tickers.update(self.data_models.out_of_market_tickers.keys())
 
         missing_tickers = all_tickers - set(df.columns)
 
         if missing_tickers:
-            logger.info("Missing tickers detected. Fetching missing data: %s", missing_tickers)
+            logger.info(f"Missing tickers detected. Fetching missing data: {missing_tickers}")
             new_data = utilities.fetch_data(all_tickers=list(missing_tickers))
             df = pd.concat([df, new_data], axis=1)
 
