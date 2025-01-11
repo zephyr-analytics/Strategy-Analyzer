f5890fc1852d8f40491d5cf6ebc61ea0567d0f29
Monica Claire W. <101022450+monicacw21@users.noreply.github.com>
2025-01-11 09:28:59 -0600

    MA crossover updated as well.

--- models/backtest_models/moving_average_crossover_processor.py
+++ models/backtest_models/moving_average_crossover_processor.py
@@ -3,6 +3,7 @@ Moving Average Crossover Backtesting Processor.
 """
 
 import logging
+import datetime
 
 import pandas as pd
 
@@ -39,7 +40,12 @@ class MovingAverageCrossoverProcessor(BacktestingProcessor):
         Processes the backtest by fetching data, running the backtest, and generating the plots.
         """
         logger.info(f"Moving Average Crossover backtest for: {self.weights_filename}, Trading Freq:{self.trading_frequency}, Fast MA:{self.fast_ma_period}, Slow MA:{self.slow_ma_period}, Type:{self.ma_type}")
-        self.run_backtest()
+        all_adjusted_weights, portfolio_values, portfolio_returns = self.run_backtest()
+        self._persist_portfolio_data(
+            all_adjusted_weights=all_adjusted_weights,
+            portfolio_values=portfolio_values,
+            portfolio_returns=portfolio_returns
+        )
         self._get_portfolio_statistics()
         self._calculate_buy_and_hold()
         self._calculate_benchmark()
@@ -51,6 +57,16 @@ class MovingAverageCrossoverProcessor(BacktestingProcessor):
             results_processor.plot_returns_heatmaps()
         else:
             pass
+
+    def get_portfolio_assets_and_weights(self, current_date):
+        """
+        """
+        adjusted_weights = self.adjust_weights(current_date=current_date)
+
+        return adjusted_weights
+
+    def calculate_momentum(self, current_date: datetime=None):
+        pass
 
     def calculate_moving_averages(self, data, period):
         """
@@ -143,61 +159,3 @@ class MovingAverageCrossoverProcessor(BacktestingProcessor):
                 adjusted_weights[ticker] /= total_weight
 
         return adjusted_weights
-
-    def run_backtest(self):
-        """
-        Runs the moving average crossover backtest.
-        """
-        monthly_dates = pd.date_range(start=self.start_date, end=self.end_date, freq='M')
-        portfolio_values = [self.initial_portfolio_value]
-        portfolio_returns = []
-        all_adjusted_weights = []
-
-        if self.trading_frequency == "Monthly":
-            step = 1
-            freq = "M"
-        elif self.trading_frequency == "Bi-Monthly":
-            step = 2
-            freq = "2M"
-        elif self.trading_frequency == "Quarterly":
-            step = 3
-            freq = "3M"
-        elif self.trading_frequency == "Yearly":
-            step = 12
-            freq = "12M"
-        else:
-            raise ValueError("Invalid trading frequency. Choose 'Monthly', 'Bi-Monthly', 'Quarterly', or 'Yearly'.")
-
-        for i in range(0, len(monthly_dates), step):
-            current_date = monthly_dates[i]
-            next_date = monthly_dates[min(i + step, len(monthly_dates) - 1)]
-            last_date_current_month = self.trading_data.index[self.trading_data.index.get_loc(current_date, method='pad')]
-            adjusted_weights = self.adjust_weights(last_date_current_month)
-            previous_value = portfolio_values[-1]
-            month_end_data = self.trading_data.loc[last_date_current_month]
-
-            last_date_next_month = self.trading_data.index[self.trading_data.index.get_loc(next_date, method='pad')]
-            next_month_end_data = self.trading_data.loc[last_date_next_month]
-
-            monthly_returns = next_month_end_data / month_end_data - 1
-            month_return = sum(
-                [monthly_returns.get(ticker, 0) * weight for ticker, weight in adjusted_weights.items()]
-            )
-            new_portfolio_value = previous_value * (1 + month_return)
-
-            all_adjusted_weights.append(adjusted_weights)
-            portfolio_values.append(new_portfolio_value)
-            portfolio_returns.append(month_return)
-
-        self.data_models.adjusted_weights = pd.Series(
-            all_adjusted_weights,
-            index=pd.date_range(start=self.start_date, periods=len(all_adjusted_weights), freq=freq)
-        )
-        self.data_models.portfolio_values = pd.Series(
-            portfolio_values,
-            index=pd.date_range(start=self.start_date, periods=len(portfolio_values), freq=freq)
-        )
-        self.data_models.portfolio_returns = pd.Series(
-            portfolio_returns,
-            index=pd.date_range(start=self.start_date, periods=len(portfolio_returns), freq=freq)
-        )
