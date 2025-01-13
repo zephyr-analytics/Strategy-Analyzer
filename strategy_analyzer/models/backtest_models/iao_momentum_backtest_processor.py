7ff61d082751d823496f123f0e7dbf11fae0c84a
Monica Claire W. <101022450+monicacw21@users.noreply.github.com>
2025-01-12 20:08:18 -0600

    -Clean backtesting.

--- strategy_analyzer/models/backtest_models/iao_momentum_backtest_processor.py
+++ strategy_analyzer/models/backtest_models/iao_momentum_backtest_processor.py
@@ -37,8 +37,14 @@ class IAOMomentumBacktestProcessor(BacktestingProcessor):
         """
         """
         in_market_momentum, out_of_market_momentum = self.calculate_momentum(current_date=current_date)
-        selected_assets = pd.DataFrame({'Asset': in_market_momentum.nlargest(self.num_assets_to_select).index, 'Momentum': in_market_momentum.nlargest(self.num_assets_to_select).values})
-        selected_out_of_market_asset =pd.DataFrame({'Asset': out_of_market_momentum.nlargest(1).index, 'Momentum': out_of_market_momentum.nlargest(1).values})
+        selected_assets = pd.DataFrame({
+            'Asset': in_market_momentum.nlargest(self.data_models.num_assets_to_select).index,
+            'Momentum': in_market_momentum.nlargest(self.data_models.num_assets_to_select).values
+        })
+        selected_out_of_market_asset =pd.DataFrame({
+            'Asset': out_of_market_momentum.nlargest(1).index,
+            'Momentum': out_of_market_momentum.nlargest(1).values
+        })
 
         adjusted_weights = self.adjust_weights(
             current_date=current_date, selected_assets=selected_assets, selected_out_of_market_asset=selected_out_of_market_asset
@@ -60,14 +66,14 @@ class IAOMomentumBacktestProcessor(BacktestingProcessor):
         tuple
             in_market_momentum and out_of_market_momentum
         """
-        momentum_data = self.asset_data.copy().pct_change().dropna()
+        momentum_data = self.data_portfolio.assets_data.copy().pct_change().dropna()
         momentum_3m = (momentum_data.loc[:current_date].iloc[-63:] + 1).prod() - 1
         momentum_6m = (momentum_data.loc[:current_date].iloc[-126:] + 1).prod() - 1
         momentum_9m = (momentum_data.loc[:current_date].iloc[-189:] + 1).prod() - 1
         momentum_12m = (momentum_data.loc[:current_date].iloc[-252:] + 1).prod() - 1
         in_market_momentum = (momentum_3m + momentum_6m + momentum_9m + momentum_12m) / 4
 
-        momentum_data_out_of_market = self.out_of_market_data.copy().pct_change().dropna()
+        momentum_data_out_of_market = self.data_portfolio.out_of_market_data.copy().pct_change().dropna()
         momentum_3m_out = (momentum_data_out_of_market.loc[:current_date].iloc[-63:] + 1).prod() - 1
         momentum_6m_out = (momentum_data_out_of_market.loc[:current_date].iloc[-126:] + 1).prod() - 1
         momentum_9m_out = (momentum_data_out_of_market.loc[:current_date].iloc[-189:] + 1).prod() - 1
@@ -119,10 +125,10 @@ class IAOMomentumBacktestProcessor(BacktestingProcessor):
 
             price = data.loc[:current_date, ticker].iloc[-1]
 
-            if self.ma_type == "SMA":
-                ma = data.loc[:current_date, ticker].rolling(window=self.ma_period).mean().iloc[-1]
-            elif self.ma_type == "EMA":
-                ma = data.loc[:current_date, ticker].ewm(span=self.ma_period).mean().iloc[-1]
+            if self.data_models.ma_type == "SMA":
+                ma = data.loc[:current_date, ticker].rolling(window=self.data_models.ma_window).mean().iloc[-1]
+            elif self.data_models.ma_type == "EMA":
+                ma = data.loc[:current_date, ticker].ewm(span=self.data_models.ma_window).mean().iloc[-1]
             else:
                 raise ValueError("Invalid ma_type. Choose 'SMA' or 'EMA'.")
 
@@ -138,21 +144,21 @@ class IAOMomentumBacktestProcessor(BacktestingProcessor):
             weight : float
                 Weight to be allocated.
             """
-            if not is_below_ma(selected_out_of_market_asset['Asset'].iloc[0], self.out_of_market_data):
+            if not is_below_ma(selected_out_of_market_asset['Asset'].iloc[0], self.data_portfolio.out_of_market_data):
                 adjusted_weights[selected_out_of_market_asset['Asset'].iloc[0]] = adjusted_weights.get(selected_out_of_market_asset['Asset'].iloc[0], 0) + weight
-            elif not is_below_ma(self.bond_ticker, self.bond_data):
-                adjusted_weights[self.bond_ticker] = adjusted_weights.get(self.bond_ticker, 0) + weight
+            elif not is_below_ma(self.data_models.bond_ticker, self.data_portfolio.bond_data):
+                adjusted_weights[self.data_models.bond_ticker] = adjusted_weights.get(self.data_models.bond_ticker, 0) + weight
             else:
-                adjusted_weights[self.cash_ticker] = adjusted_weights.get(self.cash_ticker, 0) + weight
+                adjusted_weights[self.data_models.cash_ticker] = adjusted_weights.get(self.data_models.cash_ticker, 0) + weight
 
-        if self.ma_threshold_asset and is_below_ma(self.ma_threshold_asset, self.ma_threshold_data):
+        if self.data_models.ma_threshold_asset and is_below_ma(self.data_models.ma_threshold_asset, self.data_portfolio.ma_threshold_data):
             allocate_to_safe_asset(1.0)
             return adjusted_weights
 
         for _, row in selected_assets.iterrows():
             asset, momentum = row['Asset'], row['Momentum']
             weight = 1 / len(selected_assets)
-            if self.filter_negative_momentum and momentum <= 0 or is_below_ma(asset, self.asset_data):
+            if self.data_models.negative_mom and momentum <= 0 or is_below_ma(asset, self.data_portfolio.assets_data):
                 allocate_to_safe_asset(weight)
             else:
                 adjusted_weights[asset] = weight
--- strategy_analyzer/models/backtest_models/momentum_backtest_processor.py
+++ strategy_analyzer/models/backtest_models/momentum_backtest_processor.py
@@ -36,9 +36,11 @@ class MomentumBacktestProcessor(BacktestingProcessor):
         """
         """
         momentum = self.calculate_momentum(current_date=current_date)
-        selected_assets = pd.DataFrame({'Asset': momentum.nlargest(self.num_assets_to_select).index, 
-                                        'Momentum': momentum.nlargest(self.num_assets_to_select).values})
-        selected_assets = selected_assets[selected_assets['Asset'].isin(self.assets_weights.keys())]
+        selected_assets = pd.DataFrame({
+            'Asset': momentum.nlargest(self.data_models.num_assets_to_select).index,
+            'Momentum': momentum.nlargest(self.data_models.num_assets_to_select).values
+        })
+        selected_assets = selected_assets[selected_assets['Asset'].isin(self.data_models.assets_weights.keys())]
         adjusted_weights = self.adjust_weights(current_date=current_date, selected_assets=selected_assets)
 
         return adjusted_weights
@@ -57,7 +59,7 @@ class MomentumBacktestProcessor(BacktestingProcessor):
         pd.Series
             Series of momentum values for each asset.
         """
-        momentum_data = self.asset_data.copy().pct_change().dropna()
+        momentum_data = self.data_portfolio.assets_data.copy().pct_change().dropna()
         momentum_3m = (momentum_data.loc[:current_date].iloc[-63:] + 1).prod() - 1
         momentum_6m = (momentum_data.loc[:current_date].iloc[-126:] + 1).prod() - 1
         momentum_9m = (momentum_data.loc[:current_date].iloc[-189:] + 1).prod() - 1
@@ -94,10 +96,10 @@ class MomentumBacktestProcessor(BacktestingProcessor):
             str
                 The replacement asset ticker.
             """
-            if self.bond_ticker and self.bond_ticker in self.bond_data.columns:
-                if not is_below_ma(self.bond_ticker, self.bond_data):
-                    return self.bond_ticker
-            return self.cash_ticker if self.cash_ticker in self.cash_data.columns else None
+            if self.data_models.bond_ticker and self.data_models.bond_ticker in self.data_portfolio.bond_data.columns:
+                if not is_below_ma(self.data_models.bond_ticker, self.data_portfolio.bond_data):
+                    return self.data_models.bond_ticker
+            return self.data_models.cash_ticker if self.data_models.cash_ticker in self.data_portfolio.cash_data.columns else None
 
         # NOTE this can be a utilities method
         def is_below_ma(ticker, data):
@@ -121,18 +123,18 @@ class MomentumBacktestProcessor(BacktestingProcessor):
 
             price = data.loc[:current_date, ticker].iloc[-1]
 
-            if self.ma_type == "SMA":
-                ma = data.loc[:current_date, ticker].rolling(window=self.ma_period).mean().iloc[-1]
-            elif self.ma_type == "EMA":
-                ma = data.loc[:current_date, ticker].ewm(span=self.ma_period).mean().iloc[-1]
+            if self.data_models.ma_type == "SMA":
+                ma = data.loc[:current_date, ticker].rolling(window=self.data_models.ma_window).mean().iloc[-1]
+            elif self.data_models.ma_type == "EMA":
+                ma = data.loc[:current_date, ticker].ewm(span=self.data_models.ma_window).mean().iloc[-1]
             else:
                 raise ValueError("Invalid ma_type. Choose 'SMA' or 'EMA'.")
 
             return price < ma
 
-        if not self.ma_threshold_asset:
+        if not self.data_models.ma_threshold_asset:
             pass
-        elif is_below_ma(self.ma_threshold_asset, self.ma_threshold_data):
+        elif is_below_ma(self.data_models.ma_threshold_asset, self.data_portfolio.ma_threshold_data):
             replacement_asset = get_replacement_asset()
             if replacement_asset:
                 return {replacement_asset: 1.0}
@@ -144,7 +146,7 @@ class MomentumBacktestProcessor(BacktestingProcessor):
             asset = row['Asset']
             momentum = row['Momentum']
 
-            if (self.filter_negative_momentum and momentum <= 0) or is_below_ma(asset, self.asset_data):
+            if (self.data_models.negative_mom and momentum <= 0) or is_below_ma(asset, self.data_portfolio.assets_data):
                 replacement_asset = get_replacement_asset()
                 if replacement_asset:
                     adjusted_weights[replacement_asset] = adjusted_weights.get(replacement_asset, 0) + 1
